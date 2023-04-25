c878471f2200e8aff15778ad9feeaaa3ff8e67b4
DenShykyta <wrkacc4@gmail.com>
2023-04-25 21:45:16 +0300

    Зроблено Компоненти.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,15 @@
+import Profile from './profile/Profile';
+import user from './data/user.json';
+
+import Statistics from './statistics/Statistics';
+import data from './data/data.json';
+
+import FriendList from './FriendLis/FriendList';
+import friends from './data/friends.json'
+
+import TransactionHistory from './transactionHistory/TransactionHistory';
+import transactions from './data/transactions.json'
+
 export const App = () => {
   return (
     <div
@@ -10,7 +22,16 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+      <Profile
+  username={user.username}
+  tag={user.tag}
+  location={user.location}
+  avatar={user.avatar}
+  stats={user.stats}
+/>
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />;
     </div>
   );
 };
